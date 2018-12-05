ef3db066adb4ed52d90d5d39b3ccab74f0a96b1a
danielatoader <daniela.toader07@gmail.com>
2018-12-05 03:44:46 +0100

    Implemented routes

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 var express = require('express')
 var http = require('http')
+var path = require('path')
 var indexRouter = require('./routes/index')
 
 var port = process.argv[2]
 var app = express()
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(path.resolve(__dirname, 'public')))
 app.get('/play', indexRouter)
 
 http.createServer(app).listen(port)
