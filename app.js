fa6892844b02bbae57dce48ebf7454661b5fdf7c
RodinHaker <rodinhaker@hotmail.com>
2018-12-05 02:28:23 +0100

    Server setup

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var express = require("express");
+var http = require("http");
+
+var port = process.argv[2];
+var app = express();
+
+app.use(express.static(__dirname + "/public"));
+http.createServer(app).listen(port);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -23,12 +31,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
